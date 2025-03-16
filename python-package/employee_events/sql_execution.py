a79015e40baa75398d763d12b58ea953b83b8820
amberstarke <109953705+amberstarke@users.noreply.github.com>
2025-03-16 21:29:46 +0000

    Update sql_execution.py

--- python-package/employee_events/sql_execution.py
+++ python-package/employee_events/sql_execution.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 # Using pathlib, create a `db_path` variable
 # that points to the absolute path for the `employee_events.db` file
-#### YOUR CODE HERE
+db_path = Path(__file__).resolve().parent / 'employee_events.db'
 
 
 # OPTION 1: MIXIN
@@ -16,16 +16,31 @@ class QueryMixin:
     # that receives an sql query as a string
     # and returns the query's result
     # as a pandas dataframe
-    #### YOUR CODE HERE
+    def pandas_query(self, sql_query):
+        """Execute SQL query and return results as pandas DataFrame"""
+        with connect(db_path) as conn:
+            return pd.read_sql_query(sql_query, conn)
 
     # Define a method named `query`
     # that receives an sql_query as a string
     # and returns the query's result as
     # a list of tuples. (You will need
     # to use an sqlite3 cursor)
-    #### YOUR CODE HERE
+    def query(self, sql_query):
+        """Execute SQL query and return results as list of tuples"""
+        with connect(db_path) as conn:
+            cursor = conn.cursor()
+            cursor.execute(sql_query)
+            
+            # Only fetch results if query returns data
+            if cursor.description:  # SELECT statements have description
+                results = cursor.fetchall()
+            else:  # For INSERT/UPDATE/DELETE
+                results = []
+                conn.commit()
+                
+            return results
     
-
  
  # Leave this code unchanged
 def query(func):
